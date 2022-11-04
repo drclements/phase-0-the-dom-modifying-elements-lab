b9686f75c2782e2a035f06218b176161f3c6edb9
Daniel Clements <drclements@liberty.edu>
2022-11-04 19:46:42 -0400

    commit tree 71d874b2895cbc00f62f6016cf137cd6a9b5514c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById("main").remove()
+document.createNewE
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+newHeader.textContent = "Daniel is the champion"
+console.log(newHeader)
+
+
